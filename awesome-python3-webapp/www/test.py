37e039020dff2d13b59a32a248d13e92f489a7ab
mis-gdk <1530951440@qq.com>
2020-07-22 21:50:34 +0800

    20200722

---
+++ awesome-python3-webapp/www/test.py
@@ -0,0 +1,28 @@
+import orm
+from models import User, Blog, Comment
+
+import asyncio
+
+async def gdk_test(loop):
+    await orm.create_pool(
+        loop,
+        user="www-data",
+        password="www-data",
+        db="awesome",
+        host="192.168.0.190",
+        port=3306,
+
+    )
+
+    u = User(
+        name="Test", email="test4@example.com", passwd="1234567890", image="about:blank"
+    )
+    await u.save()
+
+
+
+# 要运行协程，需要使用事件循环
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(gdk_test(loop))
+    print("Test finished.")
